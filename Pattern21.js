476de8c2651dc484efafcfd0fd40e283bcbf72b5
Divya <divyareddy0205@gmail.com>
2024-02-13 21:59:25 -0800

    patterns

--- Pattern21.js
+++ Pattern21.js
@@ -6,19 +6,19 @@ Pattern21(4)
    4
 */
 const  Pattern21 = (n) => {
-    let pattern = '';
+    let pattern = '', count = 1;
 
     //loop through each row
     for(let i=1; i<=n; i++)//rows
     {
         // Add spaces before printing stars
-        for(let space=1; space<=i-1; space++)
+        for(let space=1; space<i; space++)
         {
             pattern += " ";//print space
         }
 
         // Add * for each row
-        for(let j=1; j<=i; j++)//cols
+        for(let j=i; j<=n; j++)//cols
         {
             pattern += j;
         }
